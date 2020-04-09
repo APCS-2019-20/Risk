cbc3f3841a4f76e63157ce9ef67064e89adc2bc7
rybla <rybla@Ryans-PC>
2020-04-08 22:47:05 -0600

    Finished Reinforcing

--- Risk/src/CodeTest.java
+++ Risk/src/CodeTest.java
@@ -8,7 +8,8 @@ public class CodeTest
 				
 				
 			
-				File territories = new File("TerritoriesInput.txt");
+				//File territories = new File("TerritoriesInput.txt");
+				File territories = new File("TerritoryInput2.txt");
 				DealingOut.makeTerritories(territories);
 				
 			
@@ -44,9 +45,10 @@ public class CodeTest
 				boi.addPlayerTerritories(DealingOut.findTerritoryByName("New England"));
 				boi.getPlayerTerritories().get(3).setNumberOfUnits(6);
 				
-				Map.printMapWithInfo();
+				//Map.printMapWithInfo();
 				
-				Battle.chooseAttack(bruh);
+				//Battle.chooseAttack(bruh);
+				Reinforcing.endOfTurn();
 				
 			}
 
