b6f82bdac58d7ab3a970fc5a30e99f050b3c1183
Emily Awsome Wizard DeLine <emily.deline@mullenmail.com>
2020-04-08 23:54:33 -0600

    Final check through with spelling and grammer so everything should be good now

--- Risk/src/RiskDirections.java
+++ Risk/src/RiskDirections.java
@@ -18,8 +18,8 @@ public class RiskDirections
 		System.out.println("");
 	
 		System.out.println("Are you ready to enter a warzone?"
-				+ "\n 1) Yes, lets got! For Life, Liberty, and the Presuit of Happiness!"
-				+ "\n 2) Nah, I would reather not ruin my friendships/relationships.");
+				+ "\n1) Yes, let's go! For Life, Liberty, and Freedom!"
+				+ "\n2) Nah, I would reather not ruin my friendships.");
 	
 		Scanner userInput = new Scanner(System.in);
 			int choiceToPlay = userInput.nextInt();
@@ -27,17 +27,17 @@ public class RiskDirections
 			if(choiceToPlay == 1)
 			{
 				System.out.println("Alrightly then! To war it is!"
-						+ "\n Do you know the rules of Risk: Civil War?"
-						+ "\n 1) Oh of course I do! I use to play Risk all the time!"
-						+ "\n 2) No..."
-						+ "\n ");
+						+ "\nDo you know the rules of Risk: Civil War?"
+						+ "\n1) Oh of course I do! I used to play Risk all the time!"
+						+ "\n2) No..."
+						+ "\n");
 				
 		
 				int knowRules = userInput.nextInt();
 		
 				if(knowRules == 1)
 				{
-					System.out.println("Great let's get started then!"
+					System.out.println("Great! Let's get started then!"
 							+ "\n ");
 					Introduction.introducePlayers();
 				}
@@ -51,7 +51,7 @@ public class RiskDirections
 			}
 			else if(choiceToPlay == 2)
 			{	
-				System.out.println("Well okay then have a fun doing...something else?");
+				System.out.println("Well okay then have a fun doing, well, something else?");
 			}
 	}
 	
@@ -60,14 +60,14 @@ public class RiskDirections
 		Introduction.introducePlayers();
 		
 		System.out.println("");
-		System.out.println("Okay, so the rules to Risk: United States can be a little bit confusing."
-							+ "\n The game makes more sence when you actually start playing the game."
-							+ "\n Let's start out with how to set up the board. Your board looks like this: ");
+		System.out.println("Okay, so the rules to Risk: Civil War can be a little bit confusing."
+							+ "\nThe game makes more sence when you actually start playing the game."
+							+ "\nLet's start out with how to set up the board. Your board looks like this: ");
 		
 							Map.printMapWithPlaceHolders();
 							
-		System.out.println("There are 8 territories you two must compete to conquer these territories in order win."
-				+ "\n These terriotires are: "
+		System.out.println("There are 8 territories you two must fight to conquer all of these territories to win."
+				+ "\nThese terriotires are: "
 				+ "\n THE WEST"
 				+ "\n FOUR CORNERS"
 				+ "\n UPPER MIDWEST"
@@ -80,30 +80,31 @@ public class RiskDirections
 		System.out.println("");
 		
 		
-		System.out.println("To Start the game each of you have to choose which territoies you wish to own."
-				+ "\n However, this will rotate between the two of you, so basically whoever gets there first gets that territory...for now...;)");
+		System.out.println("To start the game you have to choose which territoies you wish to own."
+				+ "\nHowever, this will rotate between the two of you, so basically whoever gets there first gets that territory...for now...;)");
 			biding.startingOut();	
 			turns();
 	}
 	
 	public static void turns()
 	{
-		System.out.println("Okay, since you two have placed and arranged your troops/regeins how you want here is how to actually play the game.");
-		System.out.println("During each players during their turn will attact and possibly take over the territory they are attacking. "
-				+ "\n And other player who is being attacked must defened themselves. "
-				+ "\n Then that same player will have to move around their regemins. Now remeber there has to be at least one reginime per territory."
-				+ "\n Now during your turn you can choose to either do both or to move your troops. Because you can't just attack with out moving your tropps afterwards"
-				+ "\n if you take the territory you are attacking."
-				+ "\n How you determine who wins the battle (attack) is left up to fate. The dice will be rolled by each player. If the attacking player"
-				+ "\n rolls the higher number then they calm vicotry in that battle and take the portion of the territory they are attacking. "
-				+ "\n If the defendener rolls a high number they won the battle.");
+		System.out.println("Okay, since you two have placed and arranged your troops how you want here is how to actually play the game.");
+		System.out.println("During each turn you can choose to attact and possibly take over the territory they are attacking. "
+				+ "\nAnd the other player must defened themselves. "
+				+ "\nThen the player whose turn it is can choose their troops, only once per turn. "
+				+ "\nNow there ALWAYS has to be at least one troop per territory."
+				+ "\nDuring your turn you can choose to either do both, just attack, or just move your troops."
+				+ "\n"
+				+ "\nWhat determines the winner of the battle (attack) is left up to fate. Dice will be rolled by each player. If the attacking player"
+				+ "\nrolls the higher number then they calm vicotry in that battle and take the portion of the territory they are attacking. "
+				+ "\nIf the defendener rolls a high number they won the battle.");
 		
 		Scanner userInput = new Scanner(System.in);
 		int understanding = userInput.nextInt();
 	
 		System.out.println("And that is how you basically play the game. You two get that? "
-				+ "\n 1) Yes"
-				+ "\n 2) No");
+				+ "\n1) Yes"
+				+ "\n2) No");
 		
 		if(understanding == 1)
 		{
@@ -116,8 +117,8 @@ public class RiskDirections
 			int dontUnderstand = userInput.nextInt();
 			
 			System.out.println("Okay, what do you still not fully get?"
-					+ "\n 1) How turns work"
-					+ "\n 2) I don't want to paly anymore");
+					+ "\n1) How turns work"
+					+ "\n2) I don't want to paly anymore");
 			if(dontUnderstand == 1)
 			{
 				turns();
